6c8c0ff00eec540f0925cdd0370ba59bb1041804
Your Name <enggarrahayu64@gmail.com>
2025-04-28 10:38:43 +0700

    feat: add ability to manage tasks (CRUD)

---
+++ app/Filament/Resources/TaskResource.php
@@ -0,0 +1,191 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\TaskResource\Pages;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Task;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\Auth;
+
+class TaskResource extends Resource
+{
+    protected static ?string $model = Task::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationLabel = 'Tasks';
+    protected static ?string $pluralModelLabel = 'Tasks';
+
+    public static function canViewAny(): bool
+    {
+        $user = Auth::user();
+
+        if (! $user) {
+            return false;
+        }
+
+        if ($user->hasRole('Admin')) {
+            return true;
+        }
+
+        return $user->can('view_any_task');
+    }
+
+    public static function canView(Model $record): bool
+    {
+        $user = Auth::user();
+
+        if (! $user) {
+            return false;
+        }
+
+        if ($user->hasRole('Admin')) {
+            return true;
+        }
+
+        return $user->can('view_task');
+    }
+
+    public static function canCreate(): bool
+    {
+        $user = Auth::user();
+
+        if (! $user) {
+            return false;
+        }
+
+        if ($user->hasRole('Admin')) {
+            return true;
+        }
+
+        return $user->can('create_task');
+    }
+
+    public static function canEdit(Model $record): bool
+    {
+        $user = Auth::user();
+
+        if (! $user) {
+            return false;
+        }
+
+        if ($user->hasRole('Admin')) {
+            return true;
+        }
+
+        return $user->can('update_task');
+    }
+
+    public static function canDelete(Model $record): bool
+    {
+        $user = Auth::user();
+
+        if (! $user) {
+            return false;
+        }
+
+        if ($user->hasRole('Admin')) {
+            return true;
+        }
+
+        return $user->can('delete_task');
+    }
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Select::make('project_id')
+                    ->label('Project')
+                    ->relationship('project', 'name')
+                    ->required(),
+
+                Forms\Components\Select::make('user_id')
+                    ->label('Assigned User')
+                    ->relationship('user', 'name')
+                    ->required(),
+
+                Forms\Components\TextInput::make('title')
+                    ->required()
+                    ->maxLength(100),
+
+                Forms\Components\Textarea::make('description')
+                    ->maxLength(250),
+
+                Forms\Components\DatePicker::make('deadline')
+                    ->label('Deadline')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('title')
+                    ->searchable()
+                    ->sortable(),
+
+                Tables\Columns\TextColumn::make('project.name')
+                    ->label('Project')
+                    ->sortable(),
+
+                Tables\Columns\TextColumn::make('user.name')
+                    ->label('Assigned User')
+                    ->sortable(),
+
+                TextColumn::make('status')
+                    ->badge()
+                    ->color(fn(string $state): string => match ($state) {
+                        'to_do' => 'warning',
+                        'in_progress' => 'info',
+                        'done' => 'success',
+                        default => 'gray',
+                    })
+                    ->sortable(),
+
+
+                Tables\Columns\TextColumn::make('deadline')
+                    ->date()
+                    ->sortable(),
+            ])
+            ->filters([
+                Tables\Filters\SelectFilter::make('status')
+                    ->options([
+                        'To Do' => 'To Do',
+                        'In Progress' => 'In Progress',
+                        'Done' => 'Done',
+                    ])
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListTasks::route('/'),
+            'create' => Pages\CreateTask::route('/create'),
+            'edit' => Pages\EditTask::route('/{record}/edit'),
+        ];
+    }
+}
